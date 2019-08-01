b3eb915ecf71e5f1b2083e151f70f3ae9a997408
Prem Prakash <prakash2ji@gmail.com>
2019-08-01 00:13:10 -0300

    try fix ci

--- spec/support/databases.rb
+++ spec/support/databases.rb
@@ -3,44 +3,46 @@ module Databases
 
   def connect_to_sqlite
     ActiveRecord::Base.establish_connection(
-      adapter:  "sqlite3",
-      database: ":memory:"
+      adapter:  'sqlite3',
+      database: ':memory:'
     )
-    load("support/models.rb")
+    load('support/models.rb')
   end
 
   def connect_to_postgres
     ActiveRecord::Base.establish_connection(
-      adapter:  "postgresql"
+      adapter:  'postgresql'
     )
-    ActiveRecord::Base.connection.recreate_database("test_active_record_union")
+    ActiveRecord::Base.connection.recreate_database('test_active_record_union')
     ActiveRecord::Base.establish_connection(
-      adapter:  "postgresql",
-      database: "test_active_record_union"
+      adapter:  'postgresql',
+      database: 'test_active_record_union'
     )
-    load("support/models.rb")
+    load('support/models.rb')
   end
 
   def connect_to_mysql
     ActiveRecord::Base.establish_connection(
-      adapter:  "mysql2"
+      adapter:  'mysql2',
+      username: 'travis'
     )
-    ActiveRecord::Base.connection.recreate_database("test_active_record_union")
+    ActiveRecord::Base.connection.recreate_database('test_active_record_union')
     ActiveRecord::Base.establish_connection(
-      adapter:  "mysql2",
-      database: "test_active_record_union"
+      adapter:  'mysql2',
+      database: 'test_active_record_union',
+      username: 'travis'
     )
-    load("support/models.rb")
+    load('support/models.rb')
   end
 
-  def with_postgres(&block)
+  def with_postgres
     connect_to_postgres
     yield
   ensure
     connect_to_sqlite
   end
 
-  def with_mysql(&block)
+  def with_mysql
     connect_to_mysql
     yield
   ensure
