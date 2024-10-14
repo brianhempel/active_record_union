3eaf2e90cc67dd03a2f34b79e31788b9dc6c9f71
Frederik Erbs Spang Thomsen <fst@progras.dk>
2024-10-14 15:42:34 +0200

    Use attr_reader

--- lib/active_record_union/active_record/relation/union.rb
+++ lib/active_record_union/active_record/relation/union.rb
@@ -21,7 +21,7 @@ module ActiveRecord
         other = if args.empty? && relation_or_where_arg.is_a?(Relation)
                   relation_or_where_arg
                 else
-                  @klass.where(relation_or_where_arg, *args)
+                  self.klass.where(relation_or_where_arg, *args)
                 end
 
         verify_relations_for_set_operation!(operation, self, other)
@@ -37,20 +37,20 @@ module ActiveRecord
         end
 
         set  = SET_OPERATION_TO_AREL_CLASS[operation].new(left, right)
-        from = Arel::Nodes::TableAlias.new(set, @klass.arel_table.name)
+        from = Arel::Nodes::TableAlias.new(set, self.klass.arel_table.name)
         build_union_relation(from, other)
       end
 
       if ActiveRecord.gem_version >= Gem::Version.new('5.2.0.beta2')
         # Since Rails 5.2, binds are maintained only in the Arel AST.
         def build_union_relation(arel_table_alias, _other)
-          @klass.unscoped.from(arel_table_alias)
+          self.klass.unscoped.from(arel_table_alias)
         end
       elsif ActiveRecord::VERSION::MAJOR >= 5
         # In Rails >= 5.0, < 5.2, binds are maintained only in ActiveRecord
         # relations and clauses.
         def build_union_relation(arel_table_alias, other)
-          relation = @klass.unscoped.spawn
+          relation = self.klass.unscoped.spawn
           relation.from_clause =
             UnionFromClause.new(arel_table_alias, nil,
                                 self.bound_attributes + other.bound_attributes)
@@ -71,7 +71,7 @@ module ActiveRecord
         # In Rails 4.x, binds are maintained in both ActiveRecord relations and
         # clauses and also in their Arel ASTs.
         def build_union_relation(arel_table_alias, other)
-          relation = @klass.unscoped.from(arel_table_alias)
+          relation = self.klass.unscoped.from(arel_table_alias)
           relation.bind_values = self.arel.bind_values + self.bind_values +
                                  other.arel.bind_values + other.bind_values
           relation
